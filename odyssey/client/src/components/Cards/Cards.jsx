f8e92a0c0b9dc882a2e33be65d2738d829f12be5
Arsenalfutbol14 <mczanaty@gmail.com>
2021-11-05 15:27:54 -0400

    freaking out

---
+++ odyssey/client/src/components/Cards/Cards.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+const useStyles = makeStyles(() => ({
+  root: { maxwidth: 365 },
+  media: {
+    height: 0,
+    paddingTop: "56.25%",
+  },
+}));
+
+export default function CardPreview(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+      
+        title="Name"
+        subheader={
+          new Date(+new Date().getFullYear())
+            .toISOString()
+            .split("T")[0]
+        }
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.url}
+        title="Name"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt
+          lobortis feugiat vivamus at augue.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
---
+++ odyssey/client/src/screens/Home/Featured.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import MainFeature from "./MainFeature";
+import Cards from "../../components/Cards/Cards";
+
+export default function Featured() {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <MainFeature />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Cards url="https://source.unsplash.com/featured/?travel" />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Cards url="https://source.unsplash.com/featured/?nature" />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Cards url="https://source.unsplash.com/featured/?city" />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Cards url="https://source.unsplash.com/featured/?forest" />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Cards url="https://source.unsplash.com/featured/?mountains" />
+      </Grid>
+      <Grid item xs={6} sm={4} md={3}>
+        <Cards url="https://source.unsplash.com/featured/?island" />
+      </Grid>
+    </Grid>
+  );
+}
